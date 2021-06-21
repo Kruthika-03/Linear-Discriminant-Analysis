6330a5d74f11787a739d575e71bcf767de3d5d6b
Kruthika Alnavar <58825386+Kruthika-03@users.noreply.github.com>
2021-06-21 13:46:43 +0530

    Update LDA_code.py

--- LDA_code.py
+++ LDA_code.py
@@ -1,3 +1,4 @@
+#Import the following libraries 
 from sklearn.datasets import load_wine
 import pandas as pd
 import numpy as np
@@ -10,19 +11,30 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
+#loading of the wine dataset from the UCL repository
 wine = load_wine()
 X = pd.DataFrame(wine.data, columns=wine.feature_names)
 y = pd.Categorical.from_codes(wine.target, wine.target_names)
+
+#displaying the shape of the dataset
 X.shape
+
+#displaying the dataset
 X.head()
+
+#classes in the dataset
 wine.target_names
+
+#create the dataframe df which contains both the features and the classes
 df = X.join(pd.Series(y, name='class'))
 
+#vector is created with mean of each feature of every class
 class_feature_means = pd.DataFrame(columns=wine.target_names)
 for c, rows in df.groupby('class'):
     class_feature_means[c] = rows.mean()
 class_feature_means
 
+#within class scatter matrix
 within_class_scatter_matrix = np.zeros((13,13))
 for c, rows in df.groupby('class'):
     rows = rows.drop(['class'], axis=1)
@@ -35,6 +47,7 @@ for index, row in rows.iterrows():
     
         within_class_scatter_matrix += s ##indentation?
       
+#between class scatter matrix    
 feature_means = df.mean()
 between_class_scatter_matrix = np.zeros((13,13))
 for c in class_feature_means:    
@@ -43,23 +56,33 @@ for c in class_feature_means:
     mc, m = class_feature_means[c].values.reshape(13,1), feature_means.values.reshape(13,1)
     
     between_class_scatter_matrix += n * (mc - m).dot((mc - m).T)
-    
+
+#Linear discriminants are obtained by solving the eigen values
 eigen_values, eigen_vectors = np.linalg.eig(np.linalg.inv(within_class_scatter_matrix).dot(between_class_scatter_matrix))
 
+#eigen value with highest value carries highest information. These are sorted and are listed in an array
 pairs = [(np.abs(eigen_values[i]), eigen_vectors[:,i]) for i in range(len(eigen_values))]
 pairs = sorted(pairs, key=lambda x: x[0], reverse=True)
 for pair in pairs:
     print(pair[0])
-    
+
+#Variance    
 eigen_value_sums = sum(eigen_values)
 print('Explained Variance')
 for i, pair in enumerate(pairs):
     print('Eigenvector {}: {}'.format(i, (pair[0]/eigen_value_sums).real))
-    
+
+#matrix W is created which contains the first 2 eigenvectors    
 w_matrix = np.hstack((pairs[0][1].reshape(13,1), pairs[1][1].reshape(13,1))).real
+
+#compute the dot product of X and W resulting in Y
 X_lda = np.array(X.dot(w_matrix))
+
+#the classes are encoded as numbers
 le = LabelEncoder()
 y = le.fit_transform(df['class'])
+
+#plot the data as a function of 2 components and each class is described using different colors
 plt.xlabel('LD 1')
 plt.ylabel('LD 2')
 plt.scatter(
